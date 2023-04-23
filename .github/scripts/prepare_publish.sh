04ccd6a3886ce872605682d6a9789bc94095324c
Excale <dev_excale@hotmail.com>
2023-04-23 19:59:47 +0200

    [Workflow] Reverted to default mkdocs build

---
+++ .github/scripts/prepare_publish.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo Creating directory \'docs\'...
+mkdir docs
+
+
+# Move all files (except dotfiles) into /docs
+
+echo Moving all files into \'docs\'...
+
+for file in *
+do
+   if [[ $file != "docs" && $file != .* ]]; then
+      mv "$file" docs/
+   fi
+done
+
+
+# Move mkdocs.yml into root
+
+echo Moving mkdocs.yml to root...
+mv .github/mkdocs/mkdocs.yml .
+
+
+# Install mkdocs and dependencies
+
+echo Installing python requirements...
+pip install -r .github/mkdocs/requirements.txt
